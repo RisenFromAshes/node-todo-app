856f5daf2880e154a39854d030e2893f4a1efa0f
RisenFromAshes <ashrafurkhan37@gmail.com>
2019-01-28 06:09:19 +0600

    mongodb uri check

--- src/server/db/mongoose.js
+++ src/server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose')
 var mongodb = require('mongodb')
 
 mongoose.Promise = global.Promise
-
+console.log('Mongodb uri : ' + process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false})
 
 
